e836832e2583c4c709d38dd8540d008ea5e017f9
Sandy <SenthilvelSU>
2020-07-27 19:00:02 +0400

    Add demo for Credit Card Fraud Detection

---
+++ CreditCardFraudDetection/Helpers/FileHelper.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCardFraudDetection.Helpers
+{
+    public static class FileHelper
+    {
+        public static string BasePath
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory + "../../../";
+            }
+        }
+
+        public static string TestDataPath
+        {
+            get
+            {
+                return BasePath + "assets/input/testData.csv";
+            }
+        }
+
+        public static string TrainDataPath
+        {
+            get
+            {
+                return BasePath + "assets/input/trainData.csv";
+            }
+        }
+
+        public static string ModelPath
+        {
+            get
+            {
+                return BasePath + "assets/output/MLModel.zip";
+            }
+        }
+
+        public static string UploadPath
+        {
+            get
+            {
+                return BasePath + "assets/upload/";
+            }
+        }
+
+        public static bool ModelExists
+        {
+            get
+            {
+                return File.Exists(ModelPath);
+            }
+        }
+    }
+}
---
+++ CreditCardFraudDetection/Models/TestDataViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CreditCardFraudDetection.Models
+{
+    public class TestDataViewModel
+    {
+        public List<ModelInput> FraudList { get; set; }
+
+        public List<ModelInput> ValidList { get; set; }
+    }
+}
